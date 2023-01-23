97a1a9fe12ff23af9ea00b931bf6612eacf61841
Mahmoud Fouad <mfapolice@gmail.com>
2023-01-23 20:41:49 +0200

    update

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -8,11 +8,12 @@ const MenuPage = () => {
     <section className='menu'>
       <PageTitle title={'EL Menu'} subTitle={ 'your taste'} />
       
-      <h4 className="heading-4">variety sweet</h4>
-      <h2 className="heading-2">at your hand</h2>
+      <h2 className="heading-4">at your hand</h2>
+      <h4 className="heading-2">variety sweet</h4>
+      <p className="menu__text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam animi magni amet esse ex rem possimus quisquam soluta numquam quaerat vero sint sit, nam, ipsum architecto qui. At, autem recusandae.</p>
       
       <div className="menu__section">
-        <img src={menuimg} alt="menu image" className="menu__section--img" />
+        {/* <img src={menuimg} alt="menu image" className="menu__section--img" /> */}
         
       </div>
       
