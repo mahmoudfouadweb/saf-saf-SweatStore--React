ec5c50480f3bb395477a2d7082212406535e2e97
Mahmoud Fouad <mfapolice@gmail.com>
2023-01-25 03:09:29 +0200

    update

--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -55,7 +55,7 @@ const ItemPage = () => {
           <section className="item__related">
             <h3 className="heading-5 frist">related products</h3>
 
-            <div className="item__related--card">
+            {/* <div className="item__related--card">
               <div className="item__related--card-img-box">
                 <img src={shop1} alt="" className="item__related--card-img" />
               </div>
@@ -85,8 +85,156 @@ const ItemPage = () => {
               </div>
               <h6 className="heading-6">choco glaze</h6>
               <p className="item__related--card--text">now with chocolate chips!</p>
+            </div> */}
+        
+        <div class="item__related--card">
+                <img src={item3} alt="House 1" class="item__related--card-img" />
+                <h5 class="item__related--card-name">Beautiful Familiy House</h5>
+                <div class="item__related--card-text">
+                    <p>USA</p>
+                </div>
+                <div class="item__related--card-text">
+                    <p>5 rooms</p>
+                </div>
+                <div class="item__related--card-category">
+                    <p>325 m<sup>2</sup></p>
+                </div>
+                <div class="item__related--card-price">
+                    <p>$1,200,000</p>
+                </div>
+                <button class="btn item__related--card-btn">Contact realtor</button>
             </div>
-        </section>
+        <div class="item__related--card">
+                <img src={item2} alt="House 1" class="item__related--card-img" />
+                <h5 class="item__related--card-name">Beautiful Familiy House</h5>
+                <div class="item__related--card-text">
+                    <p>USA</p>
+                </div>
+                <div class="item__related--card-text">
+                    <p>5 rooms</p>
+                </div>
+                <div class="item__related--card-category">
+                    <p>325 m<sup>2</sup></p>
+                </div>
+                <div class="item__related--card-price">
+                    <p>$1,200,000</p>
+                </div>
+                <button class="btn item__related--card-btn">Contact realtor</button>
+            </div>
+        <div class="item__related--card">
+                <img src={item3} alt="House 1" class="item__related--card-img" />
+                <h5 class="item__related--card-name">Beautiful Familiy House</h5>
+                <div class="item__related--card-text">
+                    <p>USA</p>
+                </div>
+                <div class="item__related--card-text">
+                    <p>5 rooms</p>
+                </div>
+                <div class="item__related--card-category">
+                    <p>325 m<sup>2</sup></p>
+                </div>
+                <div class="item__related--card-price">
+                    <p>$1,200,000</p>
+                </div>
+                <button class="btn item__related--card-btn">Contact realtor</button>
+            </div>
+
+            
+            {/* <div class="home">
+                <img src={item3} alt="House 2" class="home__img" />
+                <h5 class="home__name">Modern Glass Villa</h5>
+                <div class="home__location">
+                    <p>Canada</p>
+                </div>
+                <div class="home__rooms">
+                    <p>6 rooms</p>
+                </div>
+                <div class="home__area">
+                    <p>450 m<sup>2</sup></p>
+                </div>
+                <div class="home__price">
+                    <p>$2,750,000</p>
+                </div>
+                <button class="btn home__btn">Contact realtor</button>
+            </div>
+
+            <div class="home">
+                <img src={item3} alt="House 3" class="home__img" />
+                <h5 class="home__name">Cozy Country House</h5>
+                <div class="home__location">
+                    <p>UK</p>
+                </div>
+                <div class="home__rooms">
+                    <p>4 rooms</p>
+                </div>
+                <div class="home__area">
+                    <p>250 m<sup>2</sup></p>
+                </div>
+                <div class="home__price">
+                    <p>$850,000</p>
+                </div>
+                <button class="btn home__btn">Contact realtor</button>
+            </div>
+
+            <div class="home">
+                <img src={item3} alt="House 4" class="home__img" />
+                <h5 class="home__name">Large Rustical Villa</h5>
+                <div class="home__location">
+                    <p>Portugal</p>
+                </div>
+                <div class="home__rooms">
+                    <p>6 rooms</p>
+                </div>
+                <div class="home__area">
+                    <p>480 m<sup>2</sup></p>
+                </div>
+                <div class="home__price">
+                    <p>$1,950,000</p>
+                </div>
+                <button class="btn home__btn">Contact realtor</button>
+            </div>
+
+            <div class="home">
+                <img src={item3} alt="House 5" class="home__img" />
+                <h5 class="home__name">Majestic Palace House</h5>
+                <div class="home__location">
+                    <p>Germany</p>
+                </div>
+                <div class="home__rooms">
+                    <p>18 rooms</p>
+                </div>
+                <div class="home__area">
+                    <p>4230 m<sup>2</sup></p>
+                </div>
+                <div class="home__price">
+                    <p>$9,500,000</p>
+                </div>
+                <button class="btn home__btn">Contact realtor</button>
+            </div>
+
+            <div class="home">
+                <img src={item3} alt="House 6" class="home__img" />
+                <h5 class="home__name">Modern Familiy Apartment</h5>
+                <div class="home__location">
+                    <p>Italy</p>
+                </div>
+                <div class="home__rooms">
+                    <p>3 rooms</p>
+                </div>
+                <div class="home__area">
+                    <p>180 m<sup>2</sup></p>
+                </div>
+                <div class="home__price">
+                    <p>$600,000</p>
+                </div>
+                <button class="btn home__btn">Contact realtor</button>
+            </div> */}
+      </section>
+      
+      {/* <section class="homes">
+           
+      </section> */}
+      
     </section>
   );
 };
